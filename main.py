c703656426b713e32d9dae4648a7d0ef03349dc8
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2024-06-18 11:12:50 -0500

    Added optimization for quasisymmetry with VMEC

--- main.py
+++ main.py
@@ -1,11 +1,53 @@
 import optuna
 import argparse
 
+import os
+import numpy as np
+from matplotlib import cm
+import matplotlib.pyplot as plt
+from simsopt.mhd import Vmec, QuasisymmetryRatioResidual
+from simsopt.geo import SurfaceRZFourier
+
+def remove_extra_files(vmec):
+    try: os.remove(vmec.output_file)
+    except: pass
+    try: os.remove(vmec.input_file + '_{:03d}_{:06d}'.format(vmec.mpi.group, vmec.iter))
+    except: pass
+
+def make_plot(vmec, savefig=True, filename='surf_opt.png'):
+    ntheta = 200
+    nphi = 150*vmec.indata.nfp
+    theta = np.linspace(0,1,num=ntheta)
+    phi = np.linspace(0,1,num=nphi)
+    B = np.zeros((ntheta,nphi))
+    
+    vmec.run();vmec.write_input('input.nfp4_QH_opt'); remove_extra_files(vmec)
+    surf_opt = SurfaceRZFourier.from_vmec_input('input.nfp4_QH_opt', quadpoints_phi=phi, quadpoints_theta=theta)
+    XYZ = surf_opt.gamma()
+    
+    phi2D,theta2D = np.meshgrid(2*np.pi*phi,2*np.pi*theta)
+    for imode in range(len(vmec.wout.xn_nyq)):
+        angle = vmec.wout.xm_nyq[imode]*theta2D - vmec.wout.xn_nyq[imode]*phi2D
+        B = B + vmec.wout.bmnc[imode,-1]*np.cos(angle)
+    B_rescaled = (B - B.min()) / (B.max() - B.min())
+    
+    fig = plt.figure(figsize=(6,4))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(XYZ[:,:,0], XYZ[:,:,1], XYZ[:,:,2], facecolors=cm.jet(np.transpose(B_rescaled)), rstride=1, cstride=1, antialiased=False)
+    ax.auto_scale_xyz([0.6*XYZ.min(), 0.6*XYZ.max()], [0.6*XYZ.min(), 0.6*XYZ.max()], [0.6*XYZ.min(), 0.6*XYZ.max()])
+    ax.set_box_aspect([1, 1, 1])
+    ax.axis('off')
+    if savefig: plt.savefig(filename, bbox_inches = 'tight', pad_inches = 0, dpi=300)
+    else: plt.show()
+
 # Define the objective function
-def objective(trial):
-    x = trial.suggest_uniform('x', -10, 10)
-    y = trial.suggest_uniform('y', -10, 10)
-    return (x - 2)**2 + (y + 3)**2
+def objective(trial, vmec, qs):
+    dofs = [trial.suggest_float(f'{i}', -0.15, 0.15) for i in range(len(vmec.x))]
+    vmec.x = dofs
+    try: loss = np.log(qs.total()) + (vmec.aspect()-7)**2
+    except: loss = 1e6
+    remove_extra_files(vmec)
+    return loss
 
 def create_sampler(sampler_name, search_space=None, seed=None):
     if sampler_name == "RandomSampler":
@@ -44,12 +86,26 @@ def main():
         storage = optuna.storages.RDBStorage(args.storage)
     else:
         storage = None
+        
+    vmec = Vmec('input.nfp4_QH', verbose=False)
+    surf = vmec.boundary;surf.fix_all()
+    surf.fixed_range(mmin=0, mmax=1, nmin=-1, nmax=1, fixed=False);surf.fix("rc(0,0)")
+    qs = QuasisymmetryRatioResidual(vmec, np.linspace(0,1,5,endpoint=True), helicity_n=-1, helicity_m=1)
+
+    print(f'Initial aspect ratio: {vmec.aspect()}')
+    print(f'Initial quasisymmetry residual: {qs.total()}')
+    make_plot(vmec, savefig=True, filename='surf_init.png')
 
     study = optuna.create_study(study_name=args.study_name, direction="minimize", sampler=sampler, storage=storage, load_if_exists=True)
-    study.optimize(objective, n_trials=args.trials, timeout=args.timeout)
+    study.optimize(lambda trial: objective(trial, vmec, qs), n_trials=args.trials, timeout=args.timeout)
 
     print("Best value (loss): ", study.best_value)
     print("Best parameters: ", study.best_params)
+    
+    vmec.x = [study.best_params[f'{i}'] for i in range(len(vmec.x))]
+    print(f'Optimized aspect ratio: {vmec.aspect()}')
+    print(f'Optimized quasisymmetry residual: {qs.total()}')
+    make_plot(vmec, savefig=True, filename='surf_opt.png')
 
 if __name__ == "__main__":
     main()
